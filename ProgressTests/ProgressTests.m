04f7d20d51305589c44225aab2a994820eb12ae8
Kai INUI <lied.der.optik@gmail.com>
2014-09-15 01:31:38 +0900

    initial

---
+++ ProgressTests/ProgressTests.m
@@ -0,0 +1,34 @@
+//
+//  ProgressTests.m
+//  ProgressTests
+//
+//  Created by kaiinui on 2014/09/15.
+//  Copyright (c) 2014年 kaiinui. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ProgressTests : XCTestCase
+
+@end
+
+@implementation ProgressTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
